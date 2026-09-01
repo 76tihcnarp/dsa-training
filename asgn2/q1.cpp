d773b3b30527fe75c04d53bd284dfa74ddf0365c
shubham dhanker <shubhamdhanker234@gmail.com>
2026-09-01 20:06:55 +0530

    ques set 2

---
+++ asgn2/q1.cpp
@@ -0,0 +1,15 @@
+//q27
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    string s1,s2;
+    cin>>s1>>s2;
+    cin>>n;
+
+    // for(int i=s1[0];i<s1.length();i=i+n){
+    //     for(int j=s2[0];i<s2.length();j++){
+    //         s1.insert (i , s2[j]);
+    //     }
+    // }
+} 
